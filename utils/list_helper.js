83ce212d664cb81e6f36509cac63affe05323183
vipotin <vilma.pohjonen@gmail.com>
2020-03-24 15:51:38 +0200

    4.4 Add tests for counting total likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (total, value) => {
+    return total + value
+  }
+  return blogs.length === 0 ? 0 
+    : blogs.map(blog => blog.likes).reduce(reducer)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
