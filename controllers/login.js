50df743aa406545c1c39100682714dad6719e8fc
vipotin <vilma.pohjonen@gmail.com>
2020-03-30 14:54:21 +0300

    4.18 Add token based login

---
+++ controllers/login.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+const config = require('../utils/config')
+
+loginRouter.post('/', async (request, response, next) => {
+  try {
+    const body = request.body
+
+    const user = await User.findOne({ username: body.username })
+  
+    let passwordCorrect = false
+    if (user !== null) {
+      passwordCorrect = await bcrypt.compare(body.password, user.passwordHash)
+    }
+  
+    console.log(passwordCorrect)
+    if (!passwordCorrect) return response.status(401).json({error: 'invalid username or password'})
+   
+    const userToken = {
+      username: user.username,
+      id: user._id
+    }
+  
+    const token = jwt.sign(userToken, config.VALIDATION)
+  
+    response
+      .status(200)
+      .send({ token, username: user.username, name: user.name })
+  } catch(exeption) {
+    next(exeption)
+  }
+})
+
+module.exports = loginRouter
