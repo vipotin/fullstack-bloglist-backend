b2e50b410ad621e69841ec765af7a387a540eba7
vipotin <vilma.pohjonen@gmail.com>
2020-03-30 18:29:06 +0300

    4.20 Refactor tokenExtractor

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -26,18 +26,9 @@ blogsRouter.get('/:id', async (request, response, next) => {
   }
 })
 
-const getToken = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response, next) => {
   try {
-    const token = getToken(request)
-    const decodedToken = jwt.verify(token, process.env.VALIDATION)
+    const decodedToken = jwt.verify(request.token, process.env.VALIDATION)
     const user = await User.findById(decodedToken.id)
 
     const blog = new Blog(request.body)
